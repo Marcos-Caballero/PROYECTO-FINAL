e64895c4a9eb20db3c1d8d05faf92ba673cb9fd6
Marcos-Caballero <robayo1100@gmail.com>
2023-10-04 21:32:11 -0500

    Terminando CRUD

--- backend/src/routes/notas.js
+++ backend/src/routes/notas.js
@@ -4,12 +4,12 @@ import Notas from "../../models/nota.js";
 
 /* Rutas */
 const router = Router();
-/* Solicitud get */
+/* Recuperar data de la db */
 router.get('/notas', async(req, res) => {
     const notas = await Notas.find();
     res.send(notas)
 })
-
+/* Enviar la data a la  db */
 router.post('/notas', async(req, res) => {
     const nota = new Notas({
         title: req.body.title,
@@ -17,7 +17,36 @@ router.post('/notas', async(req, res) => {
     })
     await nota.save()
     res.send(nota)
-/*     res.status(200).send('¡Nota creada!') */
+})
+/* Recuperar data de la db por ID */
+router.get('/notas/:id', async(req, res) => {
+    const nota = await Notas.findOne({ _id: req.params.id })
+    res.send(nota)
+})
+/* Editar y actualizar data de la db */
+router.patch('/notas/:id', async(req, res) => {
+    try {
+        const nota = await Notas.findOne({ _id: req.params.id})
+        if (req.body.title) {
+            nota.title = req.body.title
+        } if(req.body.content){
+            nota.content = req.body.content
+        }
+        nota.save()
+        res.send(nota)
+    } catch (error) {
+        res.send('La nota no se ha guardado')
+    }
+})
+
+/* Editar y actualizar data de la db */
+router.delete('/notas/:id', async(req, res) => {
+    try {
+        const nota = await Notas.deleteOne({ _id: req.params.id})
+        res.send(nota)
+        } catch (error) {
+        res.send('La nota no se ha guardado')
+    }
 })
 
 export default router;
