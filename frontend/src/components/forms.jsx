d0f5248b56ec0bde6dd9271f1e7b60a5f9453185
Marcos-Caballero <robayo1100@gmail.com>
2023-10-05 21:01:50 -0500

    Agregando funcion de guardar las notas

--- frontend/src/components/forms.jsx
+++ frontend/src/components/forms.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
-
+/* Funcionalidad del formulario para guardar y actualizar data */
 function Form() {
     const [nota, setNota] = useState({
         title: "",
         content: ""
     })
-    const handleChange = (e) => {
+    const handleChange = (e) => { //Función para almacenar cambios
         let newNota = {
-            [e.target.name] : e.target.value,
-            [e.target.content] : e.target.value
+            [e.target.name] : e.target.value, //Almacena el titulo escrito
+            [e.target.name] : e.target.value //Almacena el contenido escrito
         }
-        console.log(newNota)
+        setNota({...nota,...newNota})
     }
-
+    const saveNota = async () => { //Función para guardar notas
+        await fetch('http://localhost:5000/api/notas', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(nota),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        saveNota()
+    }
     /* Aqui va la estructura del formulario para crear notas */
     return (
         <div className="card">
@@ -20,7 +33,7 @@ function Form() {
                 Crea tu nota
             </div>
             <div className="card-body">
-                <form action="">
+                <form action="" onSubmit={onSubmit}>
                     <div className="form-group mb-3">
                         <input name="title" onChange={handleChange} type="text" placeholder="Titulo" className="form-control" />
                     </div>
